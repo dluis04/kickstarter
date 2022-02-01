b900e03b0ae96bb2cdf8feba0d2b6bb2a99966c4
DragonViceCode <diegoluis1993@gmail.com>
2022-02-01 12:46:34 -0500

    adding campaign create using smart contract with lib web3

--- pages/index.js
+++ pages/index.js
@@ -28,8 +28,6 @@ class CampaignIndex extends React.Component {
         return (
             <Layout>
                 <div>
-                    <link async rel="stylesheet"
-                        href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css" />
 
                     <h3>Open Campaigns</h3>
 
