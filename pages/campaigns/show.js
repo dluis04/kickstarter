35767969b2556cd1fbc99dfdef003ded5068bb3c
Diego Luis Hernandez <dluis_04@hotmail.com>
2022-02-08 05:48:37 -0500

    Create one card per property campaign Details

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
+import web3 from '../../ethereum/web3';
 
 
 class CampaignShow extends Component {
@@ -39,12 +40,28 @@ class CampaignShow extends Component {
             description: 'The manager created this campaign and can create request to withdraw money',
             style: { overflowWrap: 'break-word' }
         },
-
-
         {
             header: minimumContribution,
             meta: 'Minimun Contribution (wei)',
             description: 'Tou must contribute at least this much wei to become anapprover'
+        },
+
+        {
+            header: requestCount,
+            meta: 'Number of Requests',
+            description: 'A request tries to withdraw money from the contract. Requests must be approved by approvers'
+        },
+
+        {
+            header: approversCount,
+            meta: 'Number of Approvers',
+            description: 'Number of people who have already donated to campaign'
+        },
+
+        {
+            header: web3.utils.fromWei(balance,'ether'),
+            meta: 'Campaign Balance (ethere)',
+            description: 'The balance is how much money this campaign has left to spend.'
         }
 
         ];
